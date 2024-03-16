2ca6fa2b06d80452af03f5e46b34d1b293626beb
Huyhocdev <163633588+Huyhocdev@users.noreply.github.com>
2024-03-16 14:50:14 +0700

    create card

---
+++ config.js
@@ -0,0 +1,12 @@
+const nameGirl = 'Tú anh';
+const giftUrl = 'http://facebook.com';
+const eventName = 'Gửi chị';
+const titleCard = 'Tặng Tú anh';
+const contentCard = 'Chị ơi, gặp được chị trong thế giới này là một điều may mắn đối với em vì thế giới này rộng lắm, gặp được chị, nếu có thể chị là một phần trong cuộc sống của em nhé .';
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
